b05d5a8183720968b058b3dbdeb330dfefad984e
Florent-Alexi <florent-alexi@hotmail.fr>
2019-04-23 11:23:37 +0200

    affichage page accueil

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Pipe } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
@@ -7,6 +7,7 @@ import { Observable } from 'rxjs/Observable';
 import { Router } from '@angular/router';
 import { Http, Response, RequestOptions, Headers, HttpModule } from '@angular/http';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
@@ -14,41 +15,30 @@ import { Http, Response, RequestOptions, Headers, HttpModule } from '@angular/ht
 })
 export class HomePage implements OnInit {
 
+
 donnees: Observable<any>;
+tableauCategories;
+
+/* EXEMPLE D'UN OBJET JSON EN ARRAY
+jsonObj = {
+    '1' : [ {"nomCat" : "title1" , "idCat" : "desc1" }],
+    '2' : [ {"nomCat" : "title2" , "idCat" : "desc2" }],
+    '3' : [ {"nomCat" : "title3" , "idCat" : "desc3" }],
+    '4' : [ {"nomCat" : "title4" , "idCat" : "desc4" }],
+    '5' : [ {"nomCat" : "title5" , "idCat" : "desc5" }]
+}*/
+
 idCategorieAPI;
+ObjectToArray;
 nomCategorieAPI;
-i;
 
 constructor(public navCtrl: NavController, public httpClient: HttpClient, private router: Router, private http: Http) { }
 
-
-/*  chargerAccueil(){
-    this.i = 1;
-    this.donnees = this.httpClient.get('http://localhost:80/RESTful_API/api/categorie/read.php');
-    this.donnees.subscribe(data => {
-      console.log('my data: ', data); //Affiche dans la consonle le deuxieme tableau renvoyé par l'API
-      while (data[this.i]){
-      this.idCategorieAPI = data[this.i]['idCat'];
-      this.nomCategorieAPI = data[this.i]['nomCat'];
-      console.log(data[this.i]['nomCat']);
-      this.i = this.i +1;
-      }
-    })
-  }*/
-
-
   ngOnInit() {
-    this.i = 1;
-    this.donnees = this.httpClient.get('http://localhost:80/RESTful_API/api/categorie/read.php');
-    this.donnees.subscribe(data => {
-      console.log('my data: ', data); //Affiche dans la consonle le deuxieme tableau renvoyé par l'API
-      while (data[this.i]){
-      this.idCategorieAPI = data[this.i]['idCat'];
-      this.nomCategorieAPI = data[this.i]['nomCat'];
-      console.log(data[this.i]['nomCat']);
-      this.i = this.i +1;
-      }
-    })
+      this.donnees = this.httpClient.get('http://localhost:80/RESTful_API/api/categorie/read.php');
+      this.donnees.subscribe(data => {
+        this.tableauCategories = data;
+      })
   }
 
 }
