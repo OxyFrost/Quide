8515f234b5ec22fc558835f5cdd2b5d07b10ed3a
Nourhene Mezrani <nourhene.mezrani@gmail.com>
2019-04-24 09:03:55 +0200

    Nourhene (#12)
    * connection api
    
    * merge
    
    * connecxion et inscription
    
    * connection
    
    * connection
    
    * connection
    
    * affichage page accueil
    
    * connection
    
    * merge

--- src/app/pages/connexion/connexion.page.ts
+++ src/app/pages/connexion/connexion.page.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClientModule, HttpClient} from '@angular/common/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {NavController, NavParams} from '@ionic/angular';
+import {Observable} from 'rxjs/Observable';
+import {Router} from '@angular/router';
+import {Http, Response, RequestOptions, Headers, HttpModule} from '@angular/http';
 
 @Component({
-  selector: 'app-connexion',
-  templateUrl: './connexion.page.html',
-  styleUrls: ['./connexion.page.scss'],
+    selector: 'app-connexion',
+    templateUrl: './connexion.page.html',
+    styleUrls: ['./connexion.page.scss'],
 })
 export class ConnexionPage implements OnInit {
 
-  constructor() { }
+    donnees: Observable<any>;
+    emailInput;
+    emailAPI;
+    mdpInput;
+    mdpAPI;
 
-  ngOnInit() {
-  }
+    constructor(public navCtrl: NavController, public httpClient: HttpClient, private router: Router, private http: Http) {
+    }
+
+
+    connexion() {
+        this.donnees = this.httpClient.get('http://localhost:80/API_QUIDE/api/utilisateur/read.php');
+        this.donnees.subscribe(data => {
+            console.log('my data: ', data); //Affiche dans la consonle le deuxieme tableau renvoyé par l'API
+        });
+    }
+
+
+    ngOnInit() {
+    }
 
 }
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Pipe } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { NavController, NavParams } from '@ionic/angular';
+import { Observable } from 'rxjs/Observable';
+import { Router } from '@angular/router';
+import { Http, Response, RequestOptions, Headers, HttpModule } from '@angular/http';
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +15,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+
+donnees: Observable<any>;
+tableauCategories;
+
+/* EXEMPLE D'UN OBJET JSON EN ARRAY
+jsonObj = {
+    '1' : [ {"nomCat" : "title1" , "idCat" : "desc1" }],
+    '2' : [ {"nomCat" : "title2" , "idCat" : "desc2" }],
+    '3' : [ {"nomCat" : "title3" , "idCat" : "desc3" }],
+    '4' : [ {"nomCat" : "title4" , "idCat" : "desc4" }],
+    '5' : [ {"nomCat" : "title5" , "idCat" : "desc5" }]
+}*/
+
+idCategorieAPI;
+ObjectToArray;
+nomCategorieAPI;
+
+constructor(public navCtrl: NavController, public httpClient: HttpClient, private router: Router, private http: Http) { }
 
   ngOnInit() {
+      this.donnees = this.httpClient.get('http://localhost:80/API_QUIDE/api/categorie/read.php');
+      this.donnees.subscribe(data => {
+        this.tableauCategories = data;
+          console.log(this.tableauCategories);
+      });
   }
 
 }
