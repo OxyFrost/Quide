12349130084438de5185c8f06c2679d023e3395b
Alec <alec.mohamedbtssio@gmail.com>
2019-04-26 11:43:13 +0200

    Page Inscription lié a Connexion et modification page profil

--- src/app/pages/inscription/inscription.page.ts
+++ src/app/pages/inscription/inscription.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClientModule, HttpClient, HttpParams} from '@angular/common/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {NavController, NavParams} from '@ionic/angular';
+import {Observable} from 'rxjs/Observable';
+import {Router} from '@angular/router';
+import {Http, Response, RequestOptions, Headers, HttpModule} from '@angular/http';
 
 @Component({
   selector: 'app-inscription',
@@ -7,17 +14,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InscriptionPage implements OnInit {
 
-    loginInput;
+    donnees: Observable<any>;
+    emailInput;
     mdpInput;
+    civilite = 'mr';
+    nomInput;
+    prenomInput;
+    telInput;
+
+    CmdpInput;
 
 
-  constructor() { }
+    constructor(public navCtrl: NavController, public httpClient: HttpClient, private router: Router, private http: Http) {
+    }
 
   ngOnInit() {
   }
 
 
   inscription() {
+
+      // Initialize Params Object
+      let params = new HttpParams();
+
+      // Begin assigning parameters
+      params = params.append('login', this.emailInput);
+      params = params.append('pwd', this.mdpInput);
+      params = params.append('civilite', this.civilite);
+      params = params.append('nom', this.nomInput);
+      params = params.append('prenom', this.prenomInput);
+      params = params.append('tel', this.telInput);
+
+
+      this.donnees = this.httpClient.put('http://localhost:80/API_QUIDE/api/utilisateur/inscription.php', { params: params });
 
   }
 
