6036abf34fa3e3d5f3cccaecad75874c253d1869
Nourhene Mezrani <nourhene.mezrani@gmail.com>
2019-04-26 11:44:11 +0200

    Merge (#25)
    * connection api
    
    * merge
    
    * connecxion et inscription
    
    * connection
    
    * connection
    
    * connection
    
    * affichage page accueil
    
    * connection
    
    * merge
    
    * modificaton front acceuil
    
    * Creation page liste lieu
    
    * Backend Information Lieu
    
    * Partie Anissa
    
    * redirection celon click accueil / ajout de la page categorie pour afficher tous les etablissement lie a une categorie choisie
    
    * Petite Modification Home
    
    * Liaison entre les pages pour l'info des lieu
    
    * Modification connexion en cours
    
    * afficher les information detaillé de chaque resultat (toutes les fenetre accueil non pas ete terminée) (#17)
    
    * Add files via upload (#13)
    
    * Add files via upload (#18)
    
    * Add files via upload (#19)
    
    * Anissa (#20)
    
    * Nourhene (#12)
    
    * connection api
    
    * merge
    
    * connecxion et inscription
    
    * connection
    
    * connection
    
    * connection
    
    * affichage page accueil
    
    * connection
    
    * merge
    
    * Intégration page Inscription
    
    * Alexis (#23)
    
    * Anissa (#7)
    
    * nouveau Projet
    
    test
    
    * nouveau projet
    
    * Create README.md
    
    * Update README.md
    
    * ajout page connexion et inscription
    
    * home
    
    * inscription
    
    * connexion
    
    * gridhome
    
    * merge
    
    * merge
    
    * Integration
    
    * Nourhene (#12)
    
    * connection api
    
    * merge
    
    * connecxion et inscription
    
    * connection
    
    * connection
    
    * connection
    
    * affichage page accueil
    
    * connection
    
    * merge
    
    * merge
    
    * merge
    
    * Add files via upload
    
    * Add files via upload
    
    * merge liste lieux
    
    * app affichage
    
    * Audrey (#24)
    
    * Page Connexion HTML et CSS
    
    * HTML et CSS de la fiche descriptive
    
    * HTML et CSS de la fiche descriptive (modif)
    
    * Page Connexion update

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -12,9 +12,9 @@ import { Http, Response, RequestOptions, Headers, HttpModule } from '@angular/ht
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
+
 })
 export class HomePage implements OnInit {
-
 
 donnees: Observable<any>;
 tableauCategories;
@@ -28,18 +28,12 @@ jsonObj = {
     '5' : [ {"nomCat" : "title5" , "idCat" : "desc5" }]
 }*/
 
-idCategorieAPI;
-ObjectToArray;
-nomCategorieAPI;
-
 constructor(public navCtrl: NavController, public httpClient: HttpClient, private router: Router, private http: Http) { }
 
   ngOnInit() {
-      this.donnees = this.httpClient.get('http://localhost:80/API_QUIDE/api/categorie/read.php');
+      this.donnees = this.httpClient.get('http://localhost/API_QUIDE/api/categorie/read.php');
       this.donnees.subscribe(data => {
         this.tableauCategories = data;
-          console.log(this.tableauCategories);
       });
   }
-
 }
