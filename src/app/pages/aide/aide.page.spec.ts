c838ba7645c78cbbec968af2257aa213818580ef
Nourhene Mezrani <nourhene.mezrani@gmail.com>
2019-05-28 15:23:06 +0200

    Anissa (#35)
    * connection api
    
    * merge
    
    * connecxion et inscription
    
    * connection
    
    * connection
    
    * connection
    
    * affichage page accueil
    
    * connection
    
    * merge
    
    * modificaton front acceuil
    
    * Creation page liste lieu
    
    * Backend Information Lieu
    
    * Partie Anissa
    
    * redirection celon click accueil / ajout de la page categorie pour afficher tous les etablissement lie a une categorie choisie
    
    * Petite Modification Home
    
    * Liaison entre les pages pour l'info des lieu
    
    * Modification connexion en cours
    
    * afficher les information detaillé de chaque resultat (toutes les fenetre accueil non pas ete terminée) (#17)
    
    * Add files via upload (#13)
    
    * Add files via upload (#18)
    
    * Add files via upload (#19)
    
    * Anissa (#20)
    
    * Nourhene (#12)
    
    * connection api
    
    * merge
    
    * connecxion et inscription
    
    * connection
    
    * connection
    
    * connection
    
    * affichage page accueil
    
    * connection
    
    * merge
    
    * Intégration page Inscription
    
    * Alexis (#23)
    
    * Anissa (#7)
    
    * nouveau Projet
    
    test
    
    * nouveau projet
    
    * Create README.md
    
    * Update README.md
    
    * ajout page connexion et inscription
    
    * home
    
    * inscription
    
    * connexion
    
    * gridhome
    
    * merge
    
    * merge
    
    * Integration
    
    * Nourhene (#12)
    
    * connection api
    
    * merge
    
    * connecxion et inscription
    
    * connection
    
    * connection
    
    * connection
    
    * affichage page accueil
    
    * connection
    
    * merge
    
    * merge
    
    * merge
    
    * Add files via upload
    
    * Add files via upload
    
    * merge liste lieux
    
    * Page Inscription lié a Connexion et modification page profil
    
    * Floxi (#29)
    
    * afficher les information detaillé de chaque resultat (toutes les fenetre accueil non pas ete terminée)
    
    * Changement du nom page categorie par liste-lieu
    
    * Liaison FrontEnd BackEnd + rectification
    
    * page information
    
    * test
    
    * merge dev
    
    * amélioration css
    
    * amélioration

--- src/app/pages/aide/aide.page.spec.ts
+++ src/app/pages/aide/aide.page.spec.ts
@@ -1,22 +1,22 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ProfilPage } from './profil.page';
+import { AidePage } from './aide.page';
 
-describe('ProfilPage', () => {
-  let component: ProfilPage;
-  let fixture: ComponentFixture<ProfilPage>;
+describe('AidePage', () => {
+  let component: AidePage;
+  let fixture: ComponentFixture<AidePage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProfilPage ],
+      declarations: [ AidePage ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ProfilPage);
+    fixture = TestBed.createComponent(AidePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/pages/inscription/inscription.page.ts
+++ src/app/pages/inscription/inscription.page.ts
@@ -45,7 +45,8 @@ export class InscriptionPage implements OnInit {
       params = params.append('prenom', this.prenomInput);
       params = params.append('tel', this.telInput);
 
-      this.donnees = this.httpClient.put('http://localhost:80/API_QUIDE/api/utilisateur/inscription.php', { params: params });
+      this.donnees = this.httpClient.put('http://localhost:8888/API_QUIDE/api/utilisateur/inscription.php', { params: params });
+
   }
 
 }
