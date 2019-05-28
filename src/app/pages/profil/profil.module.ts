62dbaf852e9871a1322be08acb5672468bc84515
anissa170 <anissa170@hotmail.fr>
2019-05-28 15:21:48 +0200

    amélioration

--- src/app/pages/profil/profil.module.ts
+++ src/app/pages/profil/profil.module.ts
@@ -2,12 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+
 import { IonicModule } from '@ionic/angular';
-import { NavController, NavParams } from '@ionic/angular';
-import { Observable } from 'rxjs/Observable';
-import { Router } from '@angular/router';
-import { Http, Response, RequestOptions, Headers, HttpModule } from '@angular/http';
+
 import { ProfilPage } from './profil.page';
 
 const routes: Routes = [
@@ -29,18 +26,5 @@ const routes: Routes = [
 export class ProfilPageModule {
 
 
-
-  donnees: Observable<any>;
-  tableauCategories;
   
-  
-  constructor(public navCtrl: NavController, public httpClient: HttpClient, private router: Router, private http: Http) { }
-  
-    ngOnInit() {
-        this.donnees = this.httpClient.get('http://localhost:8888/API_QUIDE/api/categorie/read.php');
-        this.donnees.subscribe(data => {
-          this.tableauCategories = data;
-        });
-    }
-  }
-  
+}
